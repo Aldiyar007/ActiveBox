8934368f458e9e12aacd1acdfa7a5decd95640a9
Aldiyar007 <79399230+Aldiyar007@users.noreply.github.com>
2021-04-03 19:51:20 +0600

    finish

--- app.js
+++ app.js
@@ -1,18 +1,64 @@
 $(function () {
+    /* Fixed Header */
     let header = $('#header');
     let intro = $('#intro');
-    let introH;
+    let introH = intro.innerHeight();
     let scrollPos = $(window).scrollTop();
+    let nav = $('#nav');
+    let navToggle = $('#navToggle');
 
-    $(window).on("scroll load resize", function () {
+    checkScroll(header, introH);
+
+    $(window).on("scroll resize", function () {
         introH = intro.innerHeight();
         scrollPos = $(this).scrollTop();
 
+        checkScroll(header, introH);
+    });
+
+    function checkScroll(header, introH) {
         if (scrollPos > introH) {
             header.addClass("fixed");
         } else {
             header.removeClass("fixed");
         }
+    }
+
+    /* Smooth Scroll */
+    $('[data-scroll]').on("click", function (e) {
+        e.preventDefault();
+
+        let elementID = $(this).data('scroll');
+        let elementOffset = $(elementID).offset().top;
+
+        nav.removeClass('show');
+
+        $('html, body').animate({
+            scrollTop: elementOffset - 70
+        }, 700);
+
     });
+
+    /* Burger */
+    $('#navToggle').on("click", function (e) {
+        e.preventDefault();
+
+        nav.addClass('show');
+    })
+
+    /* Reviews */
+    let slider = $('#reviewsSlider');
+
+    slider.slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        arrows: false,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: true
+    });
+
 
 });
